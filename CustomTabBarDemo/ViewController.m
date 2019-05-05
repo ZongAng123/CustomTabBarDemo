f0165436f7246f1eea3a5b389415a5c89e4619b0
ZongAng123 <zongang15@yahoo.com>
2019-05-05 11:12:22 +0800

    Initial commit

---
+++ CustomTabBarDemo/ViewController.m
@@ -0,0 +1,24 @@
+//
+//  ViewController.m
+//  CustomTabBarDemo
+//
+//  Created by 纵昂 on 2019/5/5.
+//  Copyright © 2019 纵昂. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+    self.view.backgroundColor = [UIColor colorWithRed:arc4random()%255/255.0 green:arc4random()%255/255.0 blue:arc4random()%255/255.0 alpha:arc4random()%255/255.0];
+}
+
+
+@end
---
+++ CustomTabBarDemo/ZALineTabBarController.h
@@ -0,0 +1,17 @@
+//
+//  ZALineTabBarController.h
+//  CustomTabBarDemo
+//
+//  Created by 纵昂 on 2019/5/5.
+//  Copyright © 2019 纵昂. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface ZALineTabBarController : UITabBarController
+
+@end
+
+NS_ASSUME_NONNULL_END
